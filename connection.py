c86e36d062e2f0ce893bae54e37f32582276519f
Vanessa-21if <21vanessaaa@gmail.com>
2025-03-26 17:07:19 -0500

    contraseña

--- connection.py
+++ connection.py
@@ -3,7 +3,7 @@ from pymongo.server_api import ServerApi
 
 
 def connect_to_mongodb(db_name, collection_name):
-    uri = "mongodb+srv://21vanessaaa:<VANEifmer2025>@sampleinformationservic.ceivw.mongodb.net/"
+    uri = "mongodb+srv://21vanessaaa:VANEifmer2025@sampleinformationservic.ceivw.mongodb.net/"
     client = MongoClient(uri, server_api=ServerApi('1'))
     db = client[db_name]
     collection = db[collection_name]
