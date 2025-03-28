65e6c5ed6a2c4d2b437ef4128d16b60e52ccde9d
Vanessa-21if <21vanessaaa@gmail.com>
2025-03-28 08:51:32 -0500

    Corrigiendooouu

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, HTTPException, Request
 import uvicorn
-from app.controlador.PatientCrud import GetPatientById,WritePatient
+from app.controlador.PatientCrud import GetPatientById,WritePatient,GetPatientByIdentifier
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
 
+app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://hl7-patient-write-vanessa.onrender.com"],  # Permitir solo este dominio
@@ -12,6 +12,7 @@ app.add_middleware(
     allow_methods=["*"],  # Permitir todos los métodos (GET, POST, etc.)
     allow_headers=["*"],  # Permitir todos los encabezados
 )
+
 
 @app.get("/patient/{patient_id}", response_model=dict)
 async def get_patient_by_id(patient_id: str):
@@ -23,14 +24,16 @@ async def get_patient_by_id(patient_id: str):
     else:
         raise HTTPException(status_code=500, detail=f"Internal error. {status}")
 
-@app.get ("/patient", response_model=dict)
-async def get_patient_by_identifier(system:str, value: str):
-    status,patient= GetPatientById(system, value)
-    if status=='success':
-        return patient # Return patient id
-    elif status=='notFound':
-        raise HTTPException(status_code=500, detail=f"Internal error. {status}")
 
+@app.get("/patient", response_model=dict)
+async def get_patient_by_identifier(system: str, value: str):
+    status,patient = GetPatientByIdentifier(system,value)
+    if status=='success':
+        return patient  # Return patient
+    elif status=='notFound':
+        raise HTTPException(status_code=404, detail="Patient not found")
+    else:
+        raise HTTPException(status_code=500, detail=f"Internal error. {status}")
 
 
 @app.post("/patient", response_model=dict)
@@ -42,6 +45,8 @@ async def add_patient(request: Request):
     else:
         raise HTTPException(status_code=500, detail=f"Validating error: {status}")
 
+
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
