fc87e593ae7b6d86adfe58785c9408ea41829108
Vanessa-21if <21vanessaaa@gmail.com>
2025-05-22 18:15:01 -0500

    Update readPatient.py

--- oldFiles/readPatient.py
+++ oldFiles/readPatient.py
@@ -8,8 +8,9 @@ from dotenv import load_dotenv
 import os
 from fhir.resources.patient import Patient
 from fhir.resources.medicationdispense import MedicationDispense
-from pydantic import ValidationError
-from connection import connect_to_mongodb  
+from pydantic import ValidationError 
+from src.connection import connect_to_mongodb  
+
 
 class ClinicalRecordReader:
     """Sistema de lectura para historia clínica electrónica que cumple con la historia de usuario"""
