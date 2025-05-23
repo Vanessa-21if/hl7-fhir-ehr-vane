d0b8a443073099159ce75e261e1ae460c2a85083
Vanessa-21if <21vanessaaa@gmail.com>
2025-05-22 19:43:30 -0500

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,3 +1,4 @@
+
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
@@ -5,6 +6,6 @@ from pymongo.server_api import ServerApi
 def connect_to_mongodb(db_name, collection_name):
     uri = "mongodb+srv://21vanessaaa:VANEifmer2025@sampleinformationservic.ceivw.mongodb.net/?retryWrites=true&w=majority&appName=SampleInformationService"
     client = MongoClient(uri, server_api=ServerApi('1'))
-    db = client[]
-    collection = db[]
+    db = client[db_name]
+    collection = db[collection_name]
     return collection
