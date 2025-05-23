d425dd7eb4b0af2cebe1409658881b8232c79283
Vanessa-21if <21vanessaaa@gmail.com>
2025-05-22 23:17:12 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -149,7 +149,7 @@ if __name__ == '__main__':
         "main:app",
         host="0.0.0.0",
         port=int(os.environ.get("PORT", 8000)),
-        workers=1
+        workers=1,
         log_level="info"
     )
     
